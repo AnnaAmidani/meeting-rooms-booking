19c59b31ea78113fbb51ec0a8c8490ad73ab9e7d
Anna Amidani <an5tash@gmail.com>
2019-01-25 17:48:31 +0000

    initial boot setup

---
+++ src/main/java/com/meetingrooms/booking/meetingroomsbooking/MeetingroomsBookingApplication.java
@@ -0,0 +1,14 @@
+package com.meetingrooms.booking.meetingroomsbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingroomsBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingroomsBookingApplication.class, args);
+	}
+
+}
+
